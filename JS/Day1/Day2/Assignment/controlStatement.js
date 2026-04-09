ccce01a1f7fce21f3e555350a5446c59a28c81ce
AnmolSingh1222 <anmolkumar39078@gmail.com>
2026-04-09 14:09:56 +0530

    updated code

---
+++ JS/Day1/Day2/Assignment/controlStatement.js
@@ -0,0 +1,27 @@
+// let a = 3;
+// if(a>0){
+//     console.log("a is +ve");
+// }
+// else if(a<0){
+//     console.log("a is -ve");
+// }
+// else{
+//     console.log("a is zero");
+// }
+
+// let a =Number(prompt("E#nter your value : "))
+// console.log(typeof a)
+
+// let a = Number(prompt("Enter the value of a :"));
+// if(a<8){
+//     for(let i = 0;i < a;i ++){
+//         console.log(a);
+//     }
+// }
+// else{
+//     for(let i = 0;i < a*a;i ++){
+//         console.log(a);
+//     }
+// }
+
+
