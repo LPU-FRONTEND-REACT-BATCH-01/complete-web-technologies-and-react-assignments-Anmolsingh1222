339ff0991b6b9beb9d663736ac7176bafd876681
AnmolSingh1222 <anmolkumar39078@gmail.com>
2026-04-11 16:09:03 +0530

    updated query selector

--- JS/Day 4/Assignment/DOM.js
+++ JS/Day 4/Assignment/DOM.js
@@ -1,20 +1,58 @@
-// Data Object Model(DOM)
+//DOM-Document Object Model
 
-// document : it is an object inside our browser
-// 1. getElementById("")
-// 2. getElementByTagName("")
-// 3. getElementByClassName("")
-// 4. querySelector("")
-// 5. querySelectorAll("")
+//document-It is an object presnet inside our browser;
 
-// let heading1 = document.getElementById("heading")
-// let container1 = document.getElementsByClassName("container")
-// console.log(heading1)
-// console.log(container1)
+//1.getElementById();
+//2.getElementTagName();
+//3.getElementClassName();
+//4.querrySelector();
+//5.querrySelectorAll();
 
-// let heading = 
+// let heading = document.getElementById("heading");
 
-// let ul = document .
+// let container = document.getElementsByClassName("container");  //Html collection
+
+// let paragraph = document.getElementsByTagName("p");
+
+// let heading1 = document.querySelector("#heading");
+// let paragraph = document.querySelector("p");  //first matching will be selected
+// let conainter = document.querySelectorAll(".container");
+
+// heading1.innerHTML = "<b>This is change heading</b>";    //it will also execute html tags
+// paragraph.textContent = "<b>This is changed paragraph</b>";
+
+// heading1.classList.add("heading");
+// console.log(heading1.classList.contains("heading"));
+// heading1.classList.toggle("heading");   //if class is not present it will added it present then it remove it
+// heading1.classList.remove("heading");
+
+
+//append()
+//remove()
+//createElement()
+
+
+// let ul = document.querySelector(".fruits");
+// let kiwi = document.createElement("li")
+// let mango = document.createElement("li").textContent = "Mango";
+// kiwi.textContent = "Kiwi";
+
+// ul.after("kiwi");
+
+
+//Event Listener
+
+// let button = document.querySelector("button");
+
+// button.addEventListener('click', () => {
+//     console.log("This button got click");
+// })
+
+// button.addEventListener('mouseover', () => {
+//     console.log("This button got clicked");
+// })
+
+
 
 let button = document.querySelector("button")
 let input = document.querySelector("input")
